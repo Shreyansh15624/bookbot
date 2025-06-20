26872326d59e84ff88f394841410f826d0a81347
Shreyansh15624 <shreyanshvajroju@gmail.com>
2025-06-20 16:09:09 +0000

    7th Assignment Completed

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, sorted_dict
 
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents
         
-def main():
+def main_1():
     num_words = get_num_words(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
     num_characters = get_num_characters(get_book_text("books/frankenstein.txt"))
     for key in num_characters:
         print(f"'{key}': {num_characters[key]}")
-        
+
+def main():
+    path = "books/frankenstein.txt"
+    num_words = get_num_words(get_book_text(path))
+    num_characters = get_num_characters(get_book_text(path))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing books found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    sorted_list = sorted_dict(num_characters)
+    for item in sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+
 main()
--- stats.py
+++ stats.py
@@ -30,7 +30,6 @@ def get_num_characters(file_contents):
         for j in range(len(b)):
             if b[j] is not None:
                 file_contents_list_3.append(b[j])
-    # print(file_contents_list_3)
     for key in file_contents_list_3:
         lowered = key.lower()
         if lowered in file_contents_dict_1:
@@ -38,3 +37,14 @@ def get_num_characters(file_contents):
         else:
             file_contents_dict_1[lowered] = 1
     return file_contents_dict_1
+
+def sort_on(pair_dict):
+    return pair_dict["num"]
+
+def sorted_dict(num_characters):
+    character_list = []
+    for ch in num_characters:
+        character_list.append({"char": ch, "num": num_characters[ch]})
+    # print(character_list.sort(reverse=True, key=sort_on))
+    character_list.sort(reverse=True, key=sort_on)
+    return character_list
