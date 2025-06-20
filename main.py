37b8da9b70e2642a1edc7db75b3b481b0658bbb5
Shreyansh15624 <shreyanshvajroju@gmail.com>
2025-06-20 16:31:54 +0000

    All Assignments are Completed

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
+import sys
 from stats import get_num_words, get_num_characters, sorted_dict
 
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents
-        
-def main_1():
-    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
-    num_characters = get_num_characters(get_book_text("books/frankenstein.txt"))
-    for key in num_characters:
-        print(f"'{key}': {num_characters[key]}")
 
 def main():
-    path = "books/frankenstein.txt"
-    num_words = get_num_words(get_book_text(path))
-    num_characters = get_num_characters(get_book_text(path))
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    num_words = get_num_words(get_book_text(sys.argv[1]))
+    num_characters = get_num_characters(get_book_text(sys.argv[1]))
     print("============ BOOKBOT ============")
-    print(f"Analyzing books found at {path}...")
+    print(f"Analyzing books found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
