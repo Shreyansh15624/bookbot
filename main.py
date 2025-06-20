999f00a395e4682b34cd231a09cfbfebbe723582
Shreyansh15624 <shreyanshvajroju@gmail.com>
2025-06-20 15:26:47 +0000

    6th Assignment Completed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_characters
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,5 +8,8 @@ def get_book_text(path):
 def main():
     num_words = get_num_words(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
-    
+    num_characters = get_num_characters(get_book_text("books/frankenstein.txt"))
+    for key in num_characters:
+        print(f"'{key}': {num_characters[key]}")
+        
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,30 @@ def get_num_words(file_contents):
         if file_contents_list_1[i] not in {' ', '', '\n', None}:
             file_contents_list_2.append(file_contents_list_1[i])
     return len(file_contents_list_2)
+
+def get_num_characters(file_contents):
+    file_contents_list = file_contents.splitlines()
+    file_contents_list_1 = []
+    file_contents_list_2 = []
+    file_contents_list_3 = []
+    file_contents_dict_1 = {}
+    for i in range(0, len(file_contents_list)):
+        a = []
+        a = file_contents_list[i].split(" ")
+        for j in range(len(a)):
+            file_contents_list_1.append(a[j])
+    for i in range(len(file_contents_list_1)):
+        file_contents_list_2.append(file_contents_list_1[i])
+    for i in range(len(file_contents_list_2)):
+        b = file_contents_list_2[i]
+        for j in range(len(b)):
+            if b[j] is not None:
+                file_contents_list_3.append(b[j])
+    # print(file_contents_list_3)
+    for key in file_contents_list_3:
+        lowered = key.lower()
+        if lowered in file_contents_dict_1:
+            file_contents_dict_1[lowered] += 1
+        else:
+            file_contents_dict_1[lowered] = 1
+    return file_contents_dict_1
